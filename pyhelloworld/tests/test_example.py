6370d737bb9cae5300bdc821521aa7a851aa5924
Tobias Schraink <tobi.schraink@gmail.com>
2025-09-24 17:42:14 -0400

    remove unnecessary test_passes

--- pyhelloworld/tests/test_example.py
+++ pyhelloworld/tests/test_example.py
@@ -1,14 +1,6 @@
 from pyhelloworld import main
 
 
-def test_passes():
-    """
-    This test doesn't test anything and is just here so pytest
-    returns a 0 exit status which requires at least 1 test to be run.
-    """
-    assert True
-
-
 def test_main():
     """
     Testing that we can access functions from the python package for testing purposes.
